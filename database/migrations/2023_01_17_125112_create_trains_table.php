487c9a223a5692960a952615179136000613f6cb
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-17 14:24:06 +0100

    first commit

---
+++ database/migrations/2023_01_17_125112_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id(); // primarykey
+            $table->timestamps(); 
+            $table->string('company', 150)->nullable(); // Azienda
+            $table->string('starting_station', 200); // Stazione di partenza
+            $table->string('arrival_station', 200); // Stazione di arrivo
+            $table->dateTime('starting_time'); // Orario di partenza
+            $table->dateTime('arrival_time'); // Orario di arrivo
+            $table->tinyInteger('unique_code'); // Codice identificativo treno
+            $table->tinyInteger('carriages_num')->nullable(); // Numero di carrozze in treno
+            $table->boolean('in_time')->default(true); // Booleano che indica se il treno è in orario
+            $table->boolean('is_cancelled')->default(false); // Booleano che indica se il treno è stato cancellato
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
