b6762bba0da477a3b911a84e3fbb1bc1b39c0d16
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-17 16:22:27 +0100

    Oltre i dati inseriti manualmente su phpMyAdmin, ho aggiunto un altro record direttamente dal php

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,9 +9,22 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller {
     public function index(){
+        $new_train = new Train();
+        $new_train->company = 'Italo-Nuovo Trasporto Viaggiatori';
+        $new_train->starting_station = 'Milano Centrale';
+        $new_train->arrival_station = 'Firenze Centrale';
+        $new_train->starting_time = '2023-01-17 12:37:00';
+        $new_train->arrival_time = '2023-01-17 14:58:00';
+        $new_train->unique_code = '17824';
+        $new_train->carriages_num = 3;
+        $new_train->in_time = 1;
+        $new_train->is_cancelled = 0;
+        $new_train->save();
+
         $mytime = Carbon::now();
 
         $all_trains = Train::all();
+
         $trains = Train::where('starting_time', '>', $mytime->toDateTimeString()
         )->get();
 
