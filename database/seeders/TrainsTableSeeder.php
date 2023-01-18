b796d3d2616a798c62c65045897815c42908b127
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-18 13:54:54 +0100

    Refactoring codice Seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -18,18 +18,16 @@ class TrainsTableSeeder extends Seeder
     {
         for ($i=0; $i < 20; $i++) { 
             $newTrain = new Train();
-            $newTrain->company = $faker->randomElement(['Trenitalia SPA', 'Italo-Nuovo Trasporto Viaggiatori', 'Trenord SPA', 'Interjet Srl']);
-            $newTrain->starting_station = $faker->city();
-            $newTrain->arrival_station = $faker->city();
-            $newTrain->starting_time = $faker->dateTimeBetween('+0 days', '+2 years');
-            $newTrain->arrival_time = $faker->dateTimeBetween($newTrain->starting_time, '+2 years');;
-            $newTrain->unique_code = $faker->randomNumber(5, true);
-            $newTrain->carriages_num = $faker->numberBetween(1, 15);;
-            $newTrain->in_time = $faker->numberBetween(0, 1);
-            $newTrain->is_cancelled = $faker->numberBetween(0, 1);
-    
-            // Salvare il record nel DB
-            $newTrain->save();
+                $newTrain->company = $faker->randomElement(['Trenitalia SPA', 'Italo-Nuovo Trasporto Viaggiatori', 'Trenord SPA', 'Interjet Srl']);
+                $newTrain->starting_station = $faker->city();
+                $newTrain->arrival_station = $faker->city();
+                $newTrain->starting_time = $faker->dateTimeBetween('+0 days', '+2 years');
+                $newTrain->arrival_time = $faker->dateTimeBetween($newTrain->starting_time, '+2 years');;
+                $newTrain->unique_code = $faker->randomNumber(5, true);
+                $newTrain->carriages_num = $faker->numberBetween(1, 15);;
+                $newTrain->in_time = $faker->numberBetween(0, 1);
+                $newTrain->is_cancelled = $faker->numberBetween(0, 1);
+            $newTrain->save(); // Salvare il record nel DB
         }
     }
 }
